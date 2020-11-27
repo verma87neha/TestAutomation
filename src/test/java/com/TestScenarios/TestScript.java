c606045143a4544e826c4756a1fc86664ba21f9f
Neha Verma <verma87.neha@gmail.com>
2020-11-27 16:37:16 +0530

    test

--- src/test/java/com/TestScenarios/TestScript.java
+++ src/test/java/com/TestScenarios/TestScript.java
@@ -15,12 +15,11 @@ import com.Utility.ReadProperties;
 public class TestScript {
 	ExcelParser exlReader = new ExcelParser();
 	ReadProperties readProp = new ReadProperties();
-public static WebDriver driver = new ChromeDriver();
-	
+	public static WebDriver driver = new ChromeDriver();
 
 	@Test(dataProvider = "testData")
-	public void login(String tc, String URL,String data1,String y) {
-		System.out.println("values in login"+ " "+  tc +" "+ URL+ " "+ data1 );
+	public void login(String tc, String URL, String data1, String y) {
+		System.out.println("values in login" + " " + tc + " " + URL + " " + data1);
 		driver.get(URL);
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys(data1);
@@ -28,32 +27,32 @@ public static WebDriver driver = new ChromeDriver();
 		driver.close();
 	}
 
-@DataProvider
-public Object[][] testData()
-{try {
-	HashMap<String, String> valuesMeta = readProp.getPropValues();
-	String filePath = valuesMeta.get("filePath");
-	String TestData = valuesMeta.get("TestData");
-	String TestCase = valuesMeta.get("TestData");
-	String ObjectRepository = valuesMeta.get("TestData");
+	@DataProvider
+	public Object[][] testData() {
+		try {
+			HashMap<String, String> valuesMeta = readProp.getPropValues();
+			String filePath = valuesMeta.get("filePath");
+			String TestData = valuesMeta.get("TestData");
+			String TestCase = valuesMeta.get("TestData");
+			String ObjectRepository = valuesMeta.get("TestData");
 
-	Object[][] data = exlReader.readExcel(filePath, TestData, "TestSuit1");
+			Object[][] data = exlReader.readExcel(filePath, TestData, "TestSuit1");
 
-	return data;
-} catch (IOException e) {
-	
-	e.printStackTrace();
-	return null;
-}
-	
-}
+			return data;
+		} catch (IOException e) {
 
-@Test(dataProvider = "testData")
-public String[][] readTestData(String sno, String tc,String URL, String data1 ) {
-	
-	System.out.println("values "+ sno +" "+  tc +" "+ URL+ " "+ data1 );
-	return null;
-}
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	@Test(dataProvider = "testData")
+	public String[][] readTestData(String sno, String tc, String URL, String data1) {
+
+		System.out.println("values " + sno + " " + tc + " " + URL + " " + data1);
+		return null;
+	}
 	/*
 	 * @Test public void quit() { BrowserFactory.closeAllDriver(); }
 	 */
