c5ca4d58f9d329f045e3a486c663bf9d1d059733
Neha Verma <verma87.neha@gmail.com>
2020-11-27 16:17:13 +0530

    Initial Commit

---
+++ src/main/java/com/Utility/ObjectRepository.java
@@ -0,0 +1,40 @@
+package com.Utility;
+
+public class ObjectRepository {
+
+	public String  Image = "//*[@id=\"header_logo\"]/a/img";
+	public String email = "//*[@id=\"email_create\"]";
+	public String CreateAccount = "SubmitCreate"; //"//*[@id=\"SubmitCreate\"]";
+	public String CreateAnAccount = "//*[@id=\"noSlide\"]/h1";
+	public String FirstName = "customer_firstname";
+	public String LastName ="customer_lastname";
+	public String password = "passwd";	
+	public String FirstName_add = "firstname";
+	public String LastName_add = "lastname";
+	public String Address_add = "address1";
+	public String  City = "city";
+	public String  pincode = "postcode";
+	public String   number = "phone_mobile";
+	public String   alias = "alias";
+	public String mrs = "id_gender2";
+	public String day = "days";
+	public String months = "months";
+	public String year = "years";
+	public String state = "id_state";
+	public String Register = "submitAccount";
+	public String email_signin = "email";
+	public String password_signin = "passwd";
+	public String SignIn = "SubmitLogin";
+	public String women = "//*[@id=\"block_top_menu\"]/ul/li[1]/a";
+	public String dress = "//*[@id=\"center_column\"]/ul/li[4]/div/div[1]/div/a[1]/img";
+	public String addToCart = "add_to_cart";
+	public String checkout = "//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a";
+	public String continue_Checkout = "//*[@id=\"center_column\"]/p[2]/a[1]";
+	public String continue_Checkout_add = "processAddress";
+	public String tag_checkbox = "cgv";	
+	public String tag_shipping = "processCarrier";
+	public String payment = "bankwire";
+	public String order_payment = "//*[@id=\"cart_navigation\"]/button";
+	public String order_placed = "//*[@id=\"center_column\"]/p/a";
+
+}
